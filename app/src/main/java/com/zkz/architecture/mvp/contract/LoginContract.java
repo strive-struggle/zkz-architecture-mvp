2871151e1d9c6974c247c81ff4c0deece7b22b87
strive-struggle <13380324130@163>
2017-10-01 15:45:27 +0800

    接口完善

--- app/src/main/java/com/zkz/architecture/mvp/contract/LoginContract.java
+++ app/src/main/java/com/zkz/architecture/mvp/contract/LoginContract.java
@@ -1,5 +1,6 @@
 package com.zkz.architecture.mvp.contract;
 
+import com.zkz.architecture.mvp.model.entity.UserInfo;
 import com.zkz.architecture.mvp.view.LoginActivity;
 
 /**
@@ -8,12 +9,20 @@ import com.zkz.architecture.mvp.view.LoginActivity;
  */
 public class LoginContract {
 
+    /**
+     * 登录View层接口
+     */
     public interface ILoginView extends IBaseView {
 
     }
 
+    /**
+     * 登录Presenter层接口
+     */
     public interface ILoginPresenter extends IBasePresenter<LoginActivity> {
+        void login(UserInfo userInfo);
 
+        void logout(UserInfo userInfo);
     }
 
 }
